350d80c6ad29efd84f4235ee8fec264bddb1c2d0
beast <939212596@qq.com>
2020-03-08 16:19:43 +0800

    update

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import { ExceptionsFilter } from '../../core/filter/errors.filter'
+import { TransformInterceptor } from '../../core/interceptor/transform.interceptor'
+import { ValidationPipe } from '../../core/pipe/validation.pipe'
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -13,6 +17,10 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('admin-docs', app, document);
+
+  app.useGlobalFilters(new ExceptionsFilter())
+  app.useGlobalInterceptors(new TransformInterceptor())
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(process.env.ADMIN_PORT);
   console.log(`http://localhost:${process.env.ADMIN_PORT}/admin-docs`)
